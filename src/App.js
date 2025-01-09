29a7d4c5a56e38767dd15a1101fe479a786c5d01
KIRANRAJ <bkiranraj017@gmail.com>
2025-01-09 21:58:39 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import Home from './components/home';
 import View from "./components/views"
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
